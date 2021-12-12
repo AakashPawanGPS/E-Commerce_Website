119cde62178c48352c6d2d25873ef050b9997d83
Aakash Pawan G P S <aakashpawangps@gmail.com>
2021-12-12 22:35:32 +0530

    init

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+http.createServer(function(request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello World!');
+}).listen(port);
+
+console.log(`Server running at http://localhost:${port}`);
+
 ReactDOM.render(<App />, document.getElementById('root'));
